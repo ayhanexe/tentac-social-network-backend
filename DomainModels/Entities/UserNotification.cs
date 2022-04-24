b0b8c32b697951100547fdb1a1a6b6ad3be002e4
ayhanexe <aykhansab@code.edu.az>
2022-04-24 18:35:35 +0400

    user friend system improved

---
+++ DomainModels/Entities/UserNotification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels.Entities
+{
+    public class UserNotification : BaseEntity<int>
+    {
+        public User User { get; set; }
+        public string Text { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ Repository/Migrations/20220424132645_AddFriendsFieldToUserModel.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class AddFriendsFieldToUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserFriends_AspNetUsers_FriendId",
+                table: "UserFriends");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserFriends_FriendId",
+                table: "UserFriends");
+
+            migrationBuilder.DropColumn(
+                name: "FriendId",
+                table: "UserFriends");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Friend",
+                table: "UserFriends",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "User",
+                table: "UserFriends",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Friend",
+                table: "UserFriends");
+
+            migrationBuilder.DropColumn(
+                name: "User",
+                table: "UserFriends");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FriendId",
+                table: "UserFriends",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFriends_FriendId",
+                table: "UserFriends",
+                column: "FriendId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserFriends_AspNetUsers_FriendId",
+                table: "UserFriends",
+                column: "FriendId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
